d43d4e7047042d3adb40785affeedd7f7f69a893
Anupreet1213 <anupreet.2003@gmail.com>
2025-05-15 18:17:12 +0530

    cors update

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static("public"));
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "http://localhost:5174", "https://runic-vault.vercel.app/"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
